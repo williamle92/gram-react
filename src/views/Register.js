39fbb0d2d606836ff3d6cf994ffbe157f3b912fd
williamle92 <william1992le@gmail.com>
2021-05-31 17:09:32 -0700

    Added the register form

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,11 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
 
 export default class Register extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            redirect: null
+
+        }
+    }
+
+    // createUser = (e) => {
+    //     e.preventDefault();
+    //     let userName = e.target.username.value;
+    //     let email = e.target.email.value;
+    //     let password = e.target.password.value;
+    //     let confirmPassword = e.target.confirmPassword.value;
+    //     fetch('', {
+    //         method : 'POST',
+    //         headers : {
+    //             'Content-Type': 'application/json',
+    //             'Accept' : '*/*',
+    //             'Authorization' : 'Bearer' +
+    //         }
+            
+    //     })
+    // }
+
     render() {
         return (
             <div>
-                hello world!
+                <form>
+                    <h5>Username</h5>
+                    <input
+                        type="text"
+                        className="form-control mt-3"
+                        name="username"
+                        placeholder="Username"
+                    />
+                    <h5>Email</h5>
+                    <input
+                        type="text"
+                        className="form-control mt-3"
+                        name="email"
+                        placeholder="Email"
+                    />
+                    <h5>Password</h5>
+                    <input
+                        type="text"
+                        className="form-control mt-3"
+                        name="password"
+                        placeholder="Password"
+                    />
+                    <h5 className=''>Confirm password</h5>
+                    <input
+                        type="text"
+                        className="form-control mt-3"
+                        name="confirmpassword"
+                        placeholder="Confirm Password"
+                    />
+
+                    <button type="submit" className="btn btn-outline-info mt-3">
+                        Submit
+          </button>
+                </form>
             </div>
-        )
+        );
     }
 }
